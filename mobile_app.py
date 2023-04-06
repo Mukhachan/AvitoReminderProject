700e1c32a21c52bdb3a46aebe9162bcf39f8502f
Paul <polen1259@gmail.com>
2023-04-06 13:59:09 +0300

    переход на kivymd и немного добавлений

--- mobile_app.py
+++ mobile_app.py
@@ -1,5 +1,11 @@
-import kivy.app # импорт фрейморка kivy
-class TestApp (kivy. app. App): # формирование базового класса приложения
-    pass 
-app = TestApp () # создание объекта (приложения app) на основе базового класса 
-app.run () # запуск приложения
+from kivymd.app import MDApp 
+from kivymd.uix.label import MDLabel 
+class MainApp (MDApp):
+    def build (self): #функция вывода интерфейса
+        #self.icon = 
+        self.title = "Парсер Авито"
+        return MDLabel (text="Текст", halign="center")
+
+if __name__=='__main__':
+    app = MainApp (title="Парсер Авито") 
+    app.run()
