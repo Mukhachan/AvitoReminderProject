655bc3903924342a8e1c0abb3466ba547e7f0357
Paul <polen1259@gmail.com>
2023-04-06 15:28:21 +0300

    добавление иконки в приложение

--- mobile_app.py
+++ mobile_app.py
@@ -2,7 +2,7 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel 
 class MainApp (MDApp):
     def build (self): #функция вывода интерфейса
-        #self.icon = 
+        self.icon = "Лого1.png"
         self.title = "Парсер Авито"
         return MDLabel (text="Текст", halign="center")
 
