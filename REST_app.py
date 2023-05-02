5ef0dee4e6639e84853ed88ea750b30358fd7dde
Mukhamet1996 <aralmuhamet@gmail.com>
2023-05-02 11:50:42 +0300

    Написал REST API. Начало работы с REACT NATIVE

---
+++ REST_app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from config import db_connect_old
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+
+
+@app.route('/call_function', methods=['GET', 'POST', 'OPTIONS'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def call_function():
+    # получаем имя функции и ее аргументы из параметров запроса
+    function_name = request.args.get('function_name')
+    kwargs = {}
+    for key, value in request.args.items():
+        if key != 'function_name':
+            kwargs[key] = value
+
+    # проверяем, что имя функции задано и соответствует существующей функции
+    if function_name:
+        # получаем экземпляр класса DataBase и вызываем нужную функцию, передавая ей именованные аргументы
+        try:
+            conn = db_connect_old()
+            result = getattr(conn, function_name)(**kwargs)
+            
+            # возвращаем результат в виде JSON
+            return jsonify({'result': result})            
+        
+        except Exception:
+            return jsonify({'error': 'Invalid function name'})
+
+        finally:
+            del conn
+    else:
+        # возвращаем сообщение об ошибке в виде JSON
+        return jsonify({'error': 'Invalid function name'})
+        
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
---
+++ users.py
@@ -0,0 +1,5 @@
+from config import db_connect_old, crypt_key
+
+class Users:
+    def __init__(self) -> None:
+        pass
